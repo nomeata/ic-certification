84d791c0d1bc863f8aaf878bbb57424defeedbc8
Joachim Breitner <mail@joachim-breitner.de>
2023-03-13 11:35:21 +0100

    Add dfx to dev environment

--- flake.nix
+++ flake.nix
@@ -1,10 +1,21 @@
 {
-  inputs.nixpkgs.url        = github:NixOS/nixpkgs/master;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/master;
+  inputs.dfinity-sdk = {
+    #url = "github:paulyoung/nixpkgs-dfinity-sdk";
+    url = "github:nomeata/nixpkgs-dfinity-sdk/joachim/0.13.1";
+    #url = "/home/jojo/dfinity/nixpkgs-dfinity-sdk/";
+    flake = false;
+  };
   description = "Development environment for motoko-merkle-tree";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, dfinity-sdk }:
     let
-       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+       system = "x86_64-linux";
+
+       pkgs = import nixpkgs {
+         inherit system;
+         overlays = [ (import dfinity-sdk) ];
+       };
 
        # Switch to dfinity/vessel once
        # https://github.com/dfinity/vessel/pull/70 is merged
@@ -14,9 +25,16 @@
          rev = "99661e40c4c47110129176ee9ecc61a50f1f60db";
          hash = "sha256-nrSxy6L+wg+g9PFQJ964ZW2bZyiE/ZJ5S5N281/I6ys=";
        };
-       vessel = (import vessel-src { system = "x86_64-linux"; }).vessel;
+       vessel = (import vessel-src { inherit system; }).vessel;
+
+       dfx = (pkgs.dfinity-sdk {
+         acceptLicenseAgreement = true;
+          sdkSystem = system;
+       }).latest;
+
     in
     {
+      packages.x86_64-linux.dfx = dfx;
       devShell.x86_64-linux = pkgs.mkShell {
         buildInputs = [
           pkgs.wasmtime
@@ -26,6 +44,7 @@
               memory typed-process quickcheck-instances
             ]))
           pkgs.ghcid
+          dfx
         ];
       };
     };
