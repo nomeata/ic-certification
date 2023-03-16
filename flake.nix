8a5791d45ba3546bf76f6c347f3d7dc9de7b6951
Joachim Breitner <mail@joachim-breitner.de>
2023-03-16 12:59:00 +0100

    Add mops to nix environment

--- flake.nix
+++ flake.nix
@@ -1,40 +1,43 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/master;
   inputs.dfinity-sdk = {
-    #url = "github:paulyoung/nixpkgs-dfinity-sdk";
+#url = "github:paulyoung/nixpkgs-dfinity-sdk";
     url = "github:nomeata/nixpkgs-dfinity-sdk/joachim/0.13.1";
-    #url = "/home/jojo/dfinity/nixpkgs-dfinity-sdk/";
+#url = "/home/jojo/dfinity/nixpkgs-dfinity-sdk/";
     flake = false;
   };
+
   description = "Development environment";
 
   outputs = { self, nixpkgs, dfinity-sdk }:
     let
-       system = "x86_64-linux";
+      system = "x86_64-linux";
 
-       pkgs = import nixpkgs {
-         inherit system;
-         overlays = [ (import dfinity-sdk) ];
-       };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import dfinity-sdk) ];
+      };
 
-       # Switch to dfinity/vessel once
-       # https://github.com/dfinity/vessel/pull/70 is merged
-       vessel-src = pkgs.fetchFromGitHub {
-         owner = "dfinity";
-         repo = "vessel";
-         rev = "99661e40c4c47110129176ee9ecc61a50f1f60db";
-         hash = "sha256-nrSxy6L+wg+g9PFQJ964ZW2bZyiE/ZJ5S5N281/I6ys=";
-       };
-       vessel = (import vessel-src { inherit system; }).vessel;
+      vessel-src = pkgs.fetchFromGitHub {
+        owner = "dfinity";
+        repo = "vessel";
+        rev = "99661e40c4c47110129176ee9ecc61a50f1f60db";
+        hash = "sha256-nrSxy6L+wg+g9PFQJ964ZW2bZyiE/ZJ5S5N281/I6ys=";
+      };
+      vessel = (import vessel-src { inherit system; }).vessel;
 
-       dfx = (pkgs.dfinity-sdk {
-         acceptLicenseAgreement = true;
-          sdkSystem = system;
-       }).latest;
+      # Generated with node2nix -i <( echo '["ic-mops"]' )
+      mops = (import ./mops.nix { inherit system pkgs; }).ic-mops;
+
+      dfx = (pkgs.dfinity-sdk {
+        acceptLicenseAgreement = true;
+        sdkSystem = system;
+      }).latest;
 
     in
     {
       packages.x86_64-linux.dfx = dfx;
+      packages.x86_64-linux.mops = mops;
       devShell.x86_64-linux = pkgs.mkShell {
         buildInputs = [
           pkgs.wasmtime
@@ -46,6 +49,8 @@
           pkgs.ghcid
           dfx
           pkgs.dhall
+          pkgs.nodejs
+          mops
         ];
       };
     };
